d2dc37708e7cf74adf81af5367b12c934e9f67a1
Bunti Nizama <nizamabunti@gmail.com>
2018-03-22 01:11:59 +0530

    first commit

---
+++ chatTest_BuntiNizama/ChatViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ChatViewController.swift
+//  chatTest_BuntiNizama
+//
+//  Created by Bunti Nizama on 3/21/18.
+//  Copyright © 2018 BuntiNizama. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController,UITableViewDataSource
+{
+
+    var selectedUser : NSMutableDictionary = NSMutableDictionary()
+    @IBOutlet weak var txtViewMessage: UITextView!
+
+      @IBOutlet var tblView: UITableView!
+    //Array to store chats
+    var arrayChatData : [MessageModel]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        arrayChatData = [MessageModel]()
+        self.getChatFromServer()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func getChatFromServer()
+    {
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        NetworkUtils.GetAPI(apiName:"\(NetworkUtils.GetChat)/\(self.selectedUser.value(forKey: NetworkUtils.UserId)!)") { (status, arrayData) in
+            DispatchQueue.main.async
+            {
+                    print(arrayData)
+                    self.convertArrayToModelArray(arrayData: arrayData)
+                    MBProgressHUD.hideAllHUDs(for: self.view, animated: true)
+            }
+        }
+    }
+    
+    func convertArrayToModelArray(arrayData : NSMutableArray)
+    {
+        arrayChatData.removeAll()
+       for index in 0...arrayData.count-1
+       {
+        let msg = MessageModel(dict: arrayData[index] as! NSMutableDictionary)
+        arrayChatData.append(msg)
+        
+        }
+        tblView.reloadData()
+    }
+    
+    
+    
+    @IBAction func send()
+    {
+        
+            MBProgressHUD.showAdded(to: self.view, animated: true)
+            let dict:NSMutableDictionary  = NSMutableDictionary()
+            dict.setValue("\(self.selectedUser.value(forKey: NetworkUtils.UserId)!)", forKey: NetworkUtils.ToUserId)
+            dict.setValue(self.txtViewMessage.text, forKey: NetworkUtils.Message)
+            self.txtViewMessage.text = ""
+            self.view.endEditing(true)
+            NetworkUtils.SendChat(apiName:NetworkUtils.GetChat,params: dict as! Dictionary<String, String>) { (status, dict) in
+                
+                DispatchQueue.main.async
+                    {
+                        
+                        MBProgressHUD.hideAllHUDs(for: self.view, animated: true)
+                       
+                         self.getChatFromServer()
+                }
+                
+            }
+            
+        }
+    
+    // MARK: - UITableView Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayChatData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //Getting message Model at Index
+        let msgModelObj = arrayChatData[indexPath.row]
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatCell
+            // configure cell data
+        cell.setChatDataWithMessage(msgModel: msgModelObj)
+        cell.tag = indexPath.row
+         return cell
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ chatTest_BuntiNizama/CustomCells/ChatCell.swift
@@ -0,0 +1,112 @@
+//
+//  ChatCell.swift
+//  Hardik_HeliosTest
+//  Created by Hardik on 3/12/18.
+//  Copyright © 2018 Hardik. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+   // Outlets for label chat message and image bubble for sender and receive
+    @IBOutlet weak var lblChatMsgReceiver: UILabel!
+    @IBOutlet weak var bubbleImgSender: UIImageView!
+    @IBOutlet weak var lblChatMsgSender: UILabel!
+    @IBOutlet weak var bubbleImgReceiver: UIImageView!
+   
+    //Constraints to set width of lbl message for sender and receive
+    @IBOutlet var widthConstraintForlblMsgSender: NSLayoutConstraint!
+    @IBOutlet var widthConstraintForlblMsgReceiver: NSLayoutConstraint!
+    
+    //Constraints to set width of bubble Image for sender and receiver
+    @IBOutlet var widthConstraintForBubbleImgMsgSender: NSLayoutConstraint!
+    @IBOutlet var widthConstraintForBubbleImgMsgReceiver: NSLayoutConstraint!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //Hide all labels and images initially
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+    
+    func setChatDataWithMessage(msgModel : MessageModel)
+    {
+        //get names from message message if have
+        self.showHideBubbleAndImageForSender(sender: msgModel.isSendingMessage)
+
+        if msgModel.isSendingMessage
+        {
+            self.lblChatMsgSender.text = msgModel.chatMessage
+            self.lblChatMsgSender.sizeToFit()
+            let width = msgModel.chatMessage.width(withConstrainedHeight: self.lblChatMsgSender.frame.size.height, font: self.lblChatMsgSender.font)
+            if width > 200{
+                widthConstraintForlblMsgSender.constant = 200
+            }else{
+                widthConstraintForlblMsgSender.constant = width + 20
+//                widthConstraintForlblMsgSender.constant = msgModel.chatMessage.width(withConstrainedHeight: self.lblChatMsgSender.frame.size.height, font: self.lblChatMsgSender.font) + 20
+            }
+            setImageForMe(isMe: true, height: Float(msgModel.chatMessage.height(withConstrainedWidth: self.lblChatMsgSender.frame.size.width , font: self.lblChatMsgSender.font)))
+            widthConstraintForBubbleImgMsgSender.constant = widthConstraintForlblMsgSender.constant + self.lblChatMsgReceiver.frame.origin.x + 10
+            self.layoutSubviews()
+            
+        }else{
+            self.lblChatMsgReceiver.text = msgModel.chatMessage
+            self.lblChatMsgReceiver.sizeToFit()
+            let width = msgModel.chatMessage.width(withConstrainedHeight: self.lblChatMsgReceiver.frame.size.height, font: self.lblChatMsgReceiver.font)
+            if width > 200{
+                widthConstraintForlblMsgReceiver.constant = 200
+            }else{
+                widthConstraintForlblMsgReceiver.constant = width + 20// nameMentions.width(withConstrainedHeight: self.lblChatMsgReceiver.frame.size.height, font: self.lblChatMsgReceiver.font) + 20
+            }
+            setImageForMe(isMe: false, height: Float(msgModel.chatMessage.height(withConstrainedWidth: self.lblChatMsgReceiver.frame.size.width , font: self.lblChatMsgReceiver.font)))
+            widthConstraintForBubbleImgMsgReceiver.constant = widthConstraintForlblMsgReceiver.constant + self.lblChatMsgReceiver.frame.origin.x + 10
+            self.layoutSubviews()
+        }
+        self.layoutIfNeeded()
+    }
+    
+    //Un hide controls based on message type
+    func showHideBubbleAndImageForSender(sender : Bool)
+    {
+        self.bubbleImgSender.isHidden = !sender
+        self.lblChatMsgSender.isHidden = !sender
+        self.bubbleImgReceiver.isHidden = sender
+        self.lblChatMsgReceiver.isHidden = sender
+    }
+    //set bubble image and position
+    func setImageForMe(isMe : Bool,height : Float){
+        let marginLeft: CGFloat = 5
+        let marginRight: CGFloat = 0
+        //Bubble positions
+        var bubble_x: CGFloat = 0.0
+        let bubble_y: CGFloat = 0
+        var bubble_width: CGFloat = 0.0
+        
+        let bubble_height: Float = min(height + 8, Float(lblChatMsgSender.frame.origin.y + lblChatMsgSender.frame.size.height + 6))
+        if isMe
+        {
+            bubble_x = min(frame.origin.x - marginLeft, lblChatMsgSender.frame.origin.x - 2 * marginLeft)
+            bubbleImgSender.image? = UIImage(named: "BubbleSender")!.stretchableImage(withLeftCapWidth: 21, topCapHeight: 14)
+            
+            bubble_width = contentView.frame.size.width - bubble_x - marginRight
+            bubbleImgSender.frame = CGRect(x: bubble_x, y: bubble_y, width: bubble_width, height: CGFloat(bubble_height))
+            bubbleImgSender.autoresizingMask = autoresizingMask
+        }
+        else
+        {
+            bubble_x = marginRight
+            bubbleImgReceiver.image? = UIImage(named: "BubbleReceiver")!.stretchableImage(withLeftCapWidth: 21, topCapHeight: 14)
+            bubble_width = max(frame.origin.x + frame.size.width + marginLeft, lblChatMsgReceiver.frame.origin.x + lblChatMsgReceiver.frame.size.width + 2 * marginLeft)
+            bubbleImgReceiver.frame = CGRect(x: bubble_x, y: bubble_y, width: bubble_width , height: CGFloat(bubble_height))
+            bubbleImgReceiver.autoresizingMask = autoresizingMask
+        }
+        
+    }
+    
+}
+
+
